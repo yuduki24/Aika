b6f89f32e0f134a5d5249b19f38b02638637ea2a
yuduki <rtccb.yuduki24@gmail.com>
2017-12-06 00:29:55 +0900

    [add]fieldの表示.

--- src/puyo.py
+++ src/puyo.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import sys
 
 SCR_RECT = Rect(0, 0, 768, 576)
 CELL_SIZE = 32
@@ -12,12 +13,27 @@ class Puyopuyo:
         screen = pygame.display.set_mode(SCR_RECT.size)
         pygame.display.set_caption(u"Puyopuyo")
 
+        self.initialize(screen)
+
         clock = pygame.time.Clock()
         while True:
             clock.tick(60)
             self.draw(screen)
+            self.update()
             pygame.display.update()
             self.key_handler()
+
+    def initialize(self, screen):
+        """ゲームオブジェクトの初期化"""
+        self.all = pygame.sprite.RenderUpdates()
+        Field.containers = self.all
+        Field.screen = screen
+
+        self.field = Field(1, 1)
+
+    def update(self):
+        """ゲーム状態の更新"""
+        self.all.update()
 
     def draw(self, screen):
         """描画"""
@@ -35,7 +51,16 @@ class Puyopuyo:
         """グリッドの描画(主にデバッグ用)"""
         for y in range(WINDOW_ROW):
             for x in range(WINDOW_COL):
-                pygame.draw.rect(screen, (150, 150, 150), Rect(x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE,CELL_SIZE), 1)
+                pygame.draw.rect(screen, (150, 150, 150), Rect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
+
+class Field(pygame.sprite.Sprite):
+    colm, row = 8, 15
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.x = x
+        self.y = y
+    def update(self):
+        pygame.draw.rect(self.screen, (50, 100, 255), Rect(self.x*CELL_SIZE, self.y*CELL_SIZE, self.colm*CELL_SIZE, self.row*CELL_SIZE))
 
 if __name__ == "__main__":
     Puyopuyo()
